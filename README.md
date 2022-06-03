f0fde48e21f412b0b4d2a7627547eff6b876e203
Lavynia de Oliveira <94937715+lavyoliveira@users.noreply.github.com>
2022-06-03 18:14:53 -0300

    Criação da splash screen, da tela principal e inclusão do firebase core

---
+++ README.md
@@ -0,0 +1,3 @@
+# payflow
+
+Aplicativo em Flutter. Projeto sendo desenvolvido para a nlw da rocketseat.
---
+++ lib/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(primaryColor: AppColors.primary),
+      home: const LoginPage(),
+    );
+  }
+}
